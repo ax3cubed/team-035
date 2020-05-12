a54579c833984f6a5bf1da88db586e09d27f3f7d
Akinwole Adeola Abraham <ajunior488@gmail.com>
2020-05-12 19:47:42 +0100

    Feat: new auth router

---
+++ user.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const User = require('../models/User');
+const authMiddleware = require('../middleware/auth');
+
+const router = new express.Router();
+
+// Require controller modules
+const usersController = require('../controllers/userController');
+
+// ROUTES
+
+// Register
+router.post('/auth/register', 
+  // Middlware Chain
+  (req, res, next) => {
+   // console.log('Processing user registration with: ', req.body);
+    authMiddleware.register(req, res, next);
+  }, 
+  (req, res, next) => {
+    authMiddleware.signIn;
+    next();
+  },
+  // Handler
+  authMiddleware.signJWTForUser
+)
+
+router.post('/auth',
+  authMiddleware.signIn,
+  authMiddleware.signJWTForUser
+)
+
+// GET localhost:7000/users
+router.get('/', 
+  // authMiddleware.validateJWTManually,
+  authMiddleware.validateJWTWithPassportJWT,
+  usersController.userList
+);
+
+// POST localhost:7000/users/create
+router.post('/create',
+  // authMiddleware.validateJWTManually,
+  authMiddleware.validateJWTWithPassportJWT,
+  usersController.userCreate
+);
+
+module.exports = router;
