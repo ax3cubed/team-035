404e77b1bf7d2eceda497d615d06cd3dd24da45a
Abraham Junior <ajunior488@gmail.com>
2020-05-16 23:29:49 +0100

    feat:smart contracts compile and solidity files

--- contracts/smeih.sol
+++ contracts/smeih.sol
@@ -1,8 +1,20 @@
 pragma solidity ^0.6.7;
 
-contract smeih {
+abstract contract Updateable {
+    function run() public view virtual returns (bool);
+    function update() public virtual;
+}
+
+abstract contract Upgradable {
+    function run() public view virtual returns (bool);
+    function upgrade() public virtual;
+}
+
+contract smeih is Updateable, Upgradable{
+   event InvestmentSent( address Investor,uint uintamount);
+   constructor() public {}
     struct BusinessData{
-        address recipient;
+        address payable recipient;
         uint contributed;
         uint goal;
         uint deadline;
@@ -12,18 +24,26 @@ contract smeih {
     }
 
     struct Transaction {
-        address investor;
+        address payable investor;
         uint amount;
     }
 
     uint nextBusinessId;
     mapping(uint256 => BusinessData) businesses;
 
+    function run()
+        public
+        view
+        override(Updateable,Upgradable)
+        returns (bool) {}
+
     // Start a new business.
-    function start(address recipient, uint256 goal, uint256 deadline) returns (uint id) {
-        var business = businesses[nextBusinessId];
-        if (business.validated ==0)
-            return;
+    function start(address payable recipient, uint256 goal, uint256 deadline) public returns (uint id) {
+        BusinessData storage business = businesses[nextBusinessId];
+        require(
+          business.validated == false, "the business is not validated"
+          );
+
         business.recipient = recipient;
         business.goal = goal;
         business.deadline = deadline;
@@ -32,30 +52,32 @@ contract smeih {
     }
 
     // Contribute to the business with id $(businessId).
-    function invest(uint256 businessId) {
-        var business = businesses[businessId];
-        if (business.deadline == 0) // check for non-existing business
-            return;
-        if (business.validated == 0) // check for non-existing business
-            return;
+    function invest(uint256 businessId) public payable {
+       BusinessData storage business = businesses[businessId];
+      require(
+         now< business.deadline, "the deadline for funding has been reached "
+          );
+      require(
+          business.validated == false, "the business is not validated"
+          );
         business.contributed += msg.value;
-        var investment = business.transactions[business.num_transactions];
+        Transaction storage investment = business.transactions[business.num_transactions];
         investment.investor = msg.sender;
         investment.amount = msg.value;
         business.num_transactions++;
+        emit InvestmentSent( msg.sender,msg.value);
     }
 
     // Check whether the funding goal of the business with id $(businessId)
     // has been reached and transfer the money.
-    function checkGoalReached(uint256 businessId) returns (bool reached) {
-        var business = businesses[businessId];
+    function checkGoalReached(uint256 businessId) public returns (bool reached)  {
+        BusinessData storage business = businesses[businessId];
         if (business.deadline > 0 && business.contributed >= business.goal) {
-            business.recipient.send(business.contributed);
+            business.recipient.transfer(business.contributed);
             // clear storage, we have to do it explicitly for the mapping since
             // it is not possible to enumerate all set keys.
             for (uint i = 0; i < business.num_transactions; ++i)
                 delete business.transactions[i]; // zero out its members
-            delete business;
             reached = true;
         }
     }
@@ -63,24 +85,26 @@ contract smeih {
     // Check whether the deadline of the business with id $(businessId) has
     // passed. In that case, return the contributed money and delete the
     // business.
-    function checkExpired(uint businessId) returns (bool expired) {
+    function checkExpired(uint businessId) public returns (bool expired) {
         expired = false;
-        var business = businesses[businessId];
+         BusinessData storage business = businesses[businessId];
         if (business.deadline > 0 && block.timestamp > business.deadline) {
             // pay out the contributors
             for (uint i = 0; i < business.num_transactions; ++i) {
-                business.transactions[i].investor.send(
+                business.transactions[i].investor.transfer(
                     business.transactions[i].amount);
                 delete business.transactions[i];
             }
-            delete business;
             expired = true;
         }
     }
 
     // Return the amount contributed to the business with id $(businessId) by
     // the sender of the transaction.
-    function getContributedAmount(uint businessId) returns (uint amount) {
+    function getContributedAmount(uint businessId) public view returns (uint amount) {
         amount = businesses[businessId].contributed;
     }
+
+    function update() public override {}
+    function upgrade() public override {}
 }
